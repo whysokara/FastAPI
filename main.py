500255e4c20dd0f70a659ddfa377691bc1c06f96
Kara <kara_himanshu@yahoo.com>
2025-01-14 14:52:24 +0530

    Created post request and retrieved data

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 ## Creating an instance
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message" : "Welcome to my API.."}
+    return {"message" : "Welcome to my API"}
 
 @app.get("/posts")
 def get_posts():
     return {"data" : "This is your post"}
+
+## A post request
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...) ):
+    print(payload)
+    return {"New Post" : f"Title {payload['title']} content: {payload['content']}"}
