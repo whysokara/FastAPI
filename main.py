48470ee6ccbe9b43635b1b92aa44aae09885bc7e
Kara <kara_himanshu@yahoo.com>
2025-01-14 23:45:12 +0530

    code cleanup and basic formatting

--- main.py
+++ main.py
@@ -19,40 +19,42 @@ my_posts = [
     {"title" : "Favourite Food", "content" : "My favourite food is pizza", "id":2}
 ]
 
+## Finding Post
 def find_post(id):
     for post in my_posts:
         if post['id'] == id:
             return post
         
 ## For deleting post find index of post
-
 def find_index_post(id):
     for i, p in enumerate(my_posts):
         if p['id'] == id:
             return i
 
+## get root path
 @app.get("/")
+
 def root():
     return {"message" : "Welcome to my API"}
 
 @app.get("/posts")
+
 def get_posts():
     return {"data" : my_posts}
 
-## A post request
-
+## post request
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
+
 def create_posts(post : Post):
     post_dict = post.dict() ## converting pydantic model to python dictionary
     post_dict["id"] = randrange(1, 1000000)
     my_posts.append(post_dict)
     return {"data" : post_dict}
 
-## Get posy by id
-
+## Get post by id
 @app.get("/posts/{id}")
-def get_post(id: int):
 
+def get_post(id: int):
     post = find_post(id)
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} not found")
@@ -62,8 +64,8 @@ def get_post(id: int):
 
 
 ## Delete a post
-
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+
 def delete_post(id: int):
     index = find_index_post(id)
     if index == None:
