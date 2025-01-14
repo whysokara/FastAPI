de800e538b519157df9197f3be0a73f6bc9b0a09
Kara <kara_himanshu@yahoo.com>
2025-01-14 17:21:17 +0530

    GET request by ID

--- main.py
+++ main.py
@@ -19,6 +19,11 @@ my_posts = [
     {"title" : "Favourite Food", "content" : "My favourite food is pizza", "id":2}
 ]
 
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
+
 @app.get("/")
 def root():
     return {"message" : "Welcome to my API"}
@@ -28,9 +33,17 @@ def get_posts():
     return {"data" : my_posts}
 
 ## A post request
+
 @app.post("/posts")
 def create_posts(post : Post):
     post_dict = post.dict() ## converting pydantic model to python dictionary
-    post_dict['id'] = randrange(1, 1000000)
+    post_dict["id"] = randrange(1, 1000000)
     my_posts.append(post_dict)
     return {"data" : post_dict}
+
+## Get posy by id
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    return {"post details":post}
