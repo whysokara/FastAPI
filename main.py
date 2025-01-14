1a3b2f28f8cd88f0bee9781b5d0c5a3949425d6b
Kara <kara_himanshu@yahoo.com>
2025-01-14 14:20:21 +0530

    updated .gitignore and created first get request

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello FastAPI")
+from fastapi import FastAPI
+
+## Creating an instance
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message" : "Welcome to my API.."}
+
+@app.get("/posts")
+def get_posts():
+    return {"data" : "This is your post"}
